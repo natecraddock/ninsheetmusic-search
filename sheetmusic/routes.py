4dfc8a00f6b23d800e445b0ef688397ee632a6e6
Nathan Craddock <nzcraddock@gmail.com>
2023-01-12 21:54:20 -0700

    fix: prevent page crashing when there is no data

--- sheetmusic/routes.py
+++ sheetmusic/routes.py
@@ -1,4 +1,5 @@
 import logging
+import sqlite3
 
 from flask import Blueprint, g, render_template, request
 
@@ -9,15 +10,23 @@ routes = Blueprint("routes", __name__, static_folder="static", template_folder="
 
 def get_series() -> list[str]:
     db = get_db()
-    series = db.execute("SELECT DISTINCT series FROM music ORDER BY series ASC").fetchall()
-    if not series:
+    try:
+        series = db.execute("SELECT DISTINCT series FROM music ORDER BY series ASC").fetchall()
+        if not series:
+            return []
+        return map(lambda x: x["series"], series)
+    except sqlite3.OperationalError as e:
+        logging.error(e)
         return []
-    return map(lambda x: x["series"], series)
 
 def get_stats() -> tuple:
     db = get_db()
-    metadata = db.execute("SELECT * FROM metadata").fetchone()
-    return metadata["last_update"], metadata["num_sheets"]
+    try:
+        metadata = db.execute("SELECT * FROM metadata").fetchone()
+        return metadata["last_update"], metadata["num_sheets"]
+    except sqlite3.OperationalError as e:
+        logging.error(e)
+        return "", 0
 
 @routes.before_request
 def get_base_data():
@@ -29,6 +38,8 @@ def get_base_data():
 
 @routes.route("/")
 def index():
+    if not series:
+        return render_template
     return render_template("index.html", series=series)
 
 @routes.route("/series/<path:name>")
@@ -51,7 +62,12 @@ def series(name):
 def search():
     query = request.form["query"]
     db = get_db()
-    sheets = db.execute("SELECT * FROM music WHERE title LIKE ? OR game LIKE ?", (f"%{query}%", f"%{query}%"))
+
+    try:
+        sheets = db.execute("SELECT * FROM music WHERE title LIKE ? OR game LIKE ?", (f"%{query}%", f"%{query}%"))
+    except sqlite3.OperationalError as e:
+        logging.error(e)
+        sheets = []
 
     mapping = {}
     for sheet in sheets:
